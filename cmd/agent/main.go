c26eaea2e1c5339a5c0557a9162a63c48a1ca699
Carlos Santos <casantosmu@gmail.com>
2025-09-30 06:09:32 +0200

    Add goroutine

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"sync"
 
 	"github.com/casantosmu/port-monitor/internal/config"
 	"github.com/casantosmu/port-monitor/internal/source"
@@ -13,24 +14,37 @@ func main() {
 	flag.StringVar(&configPath, "config", "config.yaml", "path to config file")
 	flag.Parse()
 
+	log.Println("[main] starting port-monitor")
+
 	conf, err := config.LoadFromFile(configPath)
 	if err != nil {
 		log.Fatalf("[config] %s", err)
 	}
 
+	var wg sync.WaitGroup
+
 	for name, svc := range conf.Services {
-		ip, err := source.Get(svc.IPSource)
-		if err != nil {
-			log.Printf("[%s] ip_source failed: %s", name, err)
-			continue
-		}
+		wg.Add(1)
 
-		port, err := source.Get(svc.PortSource)
-		if err != nil {
-			log.Printf("[%s] port_source failed: %s", name, err)
-			continue
-		}
+		go func(name string, svc config.Service) {
+			defer wg.Done()
 
-		log.Printf("[%s] IP: %s | Port: %s", name, ip, port)
+			ip, err := source.Get(svc.IPSource)
+			if err != nil {
+				log.Printf("[%s] ip_source failed: %s", name, err)
+				return
+			}
+
+			port, err := source.Get(svc.PortSource)
+			if err != nil {
+				log.Printf("[%s] port_source failed: %s", name, err)
+				return
+			}
+
+			log.Printf("[%s] IP: %s | Port: %s", name, ip, port)
+		}(name, svc)
 	}
+
+	wg.Wait()
+	log.Println("[main] port-monitor stopped")
 }
